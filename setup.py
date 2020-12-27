dd86bb328705bebeff509eeefd8dde43abaeaea7
kmaasrud <km@aasrud.com>
2020-12-27 15:37:15 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="kodb", # Replace with your own username
+    version="0.0.1",
+    author="kmaasrud",
+    author_email="km@aasrud.com",
+    description="kmaasrud's opinionated document builder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kmaasrud/kodb",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "kodb = kodb:main"
+        ]
+    }
+)
