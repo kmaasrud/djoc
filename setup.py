13d2694f9b30dc0c372d14692ce2b20e7b4b72ba
kmaasrud <km@aasrud.com>
2021-01-14 21:52:01 +0100

    Bumped version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="kodb", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="kmaasrud",
     author_email="km@aasrud.com",
     description="kmaasrud's opinionated document builder",
