4f7532252e397adeb657d1b4615ee65181e8f62e
kmaasrud <km@aasrud.com>
2021-02-01 18:01:42 +0100

    Fixed a few NoneType bugs (I miss Rust)

--- kodb/edit_and_list.py
+++ kodb/edit_and_list.py
@@ -5,7 +5,7 @@ from kodb.utils import program_exists, find_root, find_section, style, find_proj
 
 
 # Need some way of defining your own preferred editor. Perhaps use $EDITOR? This is not Windows-friendly though...
-PREFFERED_EDITORS = ["nvim", "code", "vim", "vi", "nano"]
+PREFFERED_EDITORS = ["nvim", "code", "code-insiders", "vim", "vi", "nano"]
 
 
 def edit_project(section):
@@ -17,13 +17,13 @@ def edit_project(section):
         
     command = [editor]
         
-    if section.lower() in ["yaml", "kobd.yaml", "kodb"]:
+    if not section:
+        command.append(os.path.join(root_path, "src"))
+    elif section.lower() in ["yaml", "kobd.yaml", "kodb"]:
         command.append(os.path.join(root_path, "kodb.yaml"))
-    elif section:
+    else:
         section_path = find_section(section)
         command.append(section_path)
-    else:
-        command.append(os.path.join(root_path, "src"))
         
     subprocess.call(command)
     
@@ -40,13 +40,14 @@ def list_sections():
         with open(os.path.join(src_path, f)) as file:
             text = file.read()
             match = header_re.search(text)
-            if match.group(3) and match.group(3)[0] == "=":
-                title = match.group(2)
-            elif len(match.group(5)) == 1:
-                title = match.group(6)
+            if match:
+                if match.group(3) and match.group(3)[0] == "=":
+                    title = match.group(2)
+                elif len(match.group(5)) == 1:
+                    title = match.group(6)
         
         # If title is not found via the method above, strip the index and extension of the path and use is as title
-        title = re.sub(r"\d\d_", "", f).replace(".md", "") if not title else title
+        title = re.sub(r"\d\d_", "", f).replace(".md", "").capitalize() if not title else title
         
         print(f"\t- ({i}) {style(title, 'bold')}")
         
