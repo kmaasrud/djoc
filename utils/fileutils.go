66f13dbe1687db0c2b9696f43754f1a901a6ba96
kmaasrud <km@aasrud.com>
2021-03-31 21:25:45 +0200

    Util-func to find the root of a project

--- utils/fileutils.go
+++ utils/fileutils.go
@@ -1,8 +1,24 @@
 package utils
 
-import "os"
+import (
+    "os"
+    "path/filepath"
+    "errors"
+)
 
-func PathExists(path string) bool {
-    _, err := os.Stat(path)
-    return !os.IsNotExist(err)
+// Searches up the directory tree to find a knutex.yaml file and returns the path
+// of the directory containing it. If it reaches the root directory without finding
+// anything, FindKnutexRoot returns an error.
+func FindKnutexRoot() (string, error) {
+    path, err := os.Getwd(); CheckErr(err)
+
+    for {
+        if filepath.Dir(path) == path {
+            return "", errors.New("Could not find a KnuTeX root")
+        } else if _, err := os.Stat(filepath.Join(path, "knutex.yaml")); os.IsNotExist(err) {
+            path = filepath.Dir(path)
+        } else {
+            return path, nil
+        }
+    }
 }
