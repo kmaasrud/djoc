0ecb8796c1c564efd1c6a2e6ebf38efa336b9e99
kmaasrud <km@aasrud.com>
2021-04-05 18:50:28 +0200

    Heyho, clapper works nicely

--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-	"github.com/kmaasrud/doctor/msg"
-	"github.com/kmaasrud/doctor/utils"
-	"time"
-	"fmt"
 	"os"
+	"fmt"
+	"strings"
+	"github.com/kmaasrud/doctor/msg"
+	"github.com/kmaasrud/doctor/core"
+	// "github.com/kmaasrud/doctor/utils"
+	"github.com/thatisuday/clapper"
 )
 
 func main() {
-	/* done := make(chan struct{})
-	go thisTakesTime(done)
-	msg.Do("Doing stuff, wait a bit", "The stuff is done!", done) */
-	root, err := utils.FindDoctorRoot()
+	/* root, err := utils.FindDoctorRoot()
 	if err != nil {
 		msg.Error(err.Error())
 		os.Exit(1)
-	}
-	fmt.Println(root)
-}
+	} */
+	registry := clapper.NewRegistry()
+	registry.Register("")
 
-func thisTakesTime(done chan struct{}) {
-	time.Sleep(5 * time.Second)
-	close(done)
+	registry.Register("init")
+	newCommand, _ := registry.Register("new")
+	newCommand.AddArg("path", "doctor_document")
+
+	command, err := registry.Parse(os.Args[1:])
+	if err != nil {
+		if _, ok := err.(clapper.ErrorUnknownCommand); ok {
+			msg.Error(fmt.Sprintf("Unknown command %s. Run %s to see a list of available commands.", msg.Style(os.Args[1], "Bold"), msg.Style("doctor --help", "Bold")))
+		} else if _, ok := err.(clapper.ErrorUnknownFlag); ok {
+			errorString := fmt.Sprintf("%s%s", strings.ToUpper(string(err.Error()[0])), string(err.Error()[1:]))
+			msg.Error(fmt.Sprintf("%s. Run %s for a list of available flags", errorString, msg.Style(os.Args[1] + " --help", "Bold")))
+		}
+		os.Exit(1)
+	}
+
+	switch command.Name {
+	case "init":
+		core.CreateAt(".")
+	case "new":
+		if val := command.Args["path"].Value; val != "" {
+			core.CreateAt(command.Args["path"].Value)
+		} else {
+			core.CreateAt(command.Args["path"].DefaultValue)
+		}
+	}
 }
