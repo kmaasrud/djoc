68877b77a465ce96e7b8dc0ed709d46b15c3c8b1
kmaasrud <km@aasrud.com>
2021-04-06 16:31:05 +0200

    Parse commands: root, init and new. Some flags added

--- main.go
+++ main.go
@@ -16,31 +16,70 @@ func main() {
 		os.Exit(1)
 	} */
 	registry := clapper.NewRegistry()
-	registry.Register("")
 
-	registry.Register("init")
+	rootCommand, _ := registry.Register("")
+	rootCommand.AddFlag("check-dependencies", "", true, "")
+
+	initCommand, _ := registry.Register("init")
+	initCommand.AddFlag("default", "d", true, "")
+
 	newCommand, _ := registry.Register("new")
-	newCommand.AddArg("path", "doctor_document")
+	newCommand.AddArg("path", "")
+	newCommand.AddFlag("default", "d", true, "")
 
+
+    // Parse commands
 	command, err := registry.Parse(os.Args[1:])
+	// Handle command parsing errors
 	if err != nil {
 		if _, ok := err.(clapper.ErrorUnknownCommand); ok {
 			msg.Error(fmt.Sprintf("Unknown command %s. Run %s to see a list of available commands.", msg.Style(os.Args[1], "Bold"), msg.Style("doctor --help", "Bold")))
 		} else if _, ok := err.(clapper.ErrorUnknownFlag); ok {
 			errorString := fmt.Sprintf("%s%s", strings.ToUpper(string(err.Error()[0])), string(err.Error()[1:]))
-			msg.Error(fmt.Sprintf("%s. Run %s for a list of available flags", errorString, msg.Style(os.Args[1] + " --help", "Bold")))
+			msg.Error(fmt.Sprintf("%s. Run %s for further help.", errorString, msg.Style("kodb" + " --help", "Bold")))
+		} else {
+			msg.Error(err.Error())
 		}
 		os.Exit(1)
 	}
 
+
+	// Run the correct command logic
+    commandLogic:
 	switch command.Name {
-	case "init":
-		core.CreateDocumentAt(".")
-	case "new":
-		if val := command.Args["path"].Value; val != "" {
-			core.CreateDocumentAt(command.Args["path"].Value)
-		} else {
-			core.CreateDocumentAt(command.Args["path"].DefaultValue)
+	// Root command
+	case "":
+		for key, val := range command.Flags {
+			switch key {
+			case "check-dependencies":
+				if val.Value == "true" { core.CheckDependencies() }
+			}
 		}
+
+	case "init":
+        for key := range command.Flags {
+            switch key {
+            case "default":
+                core.CreateDocumentAt(".", true)
+                break commandLogic
+            }
+        }
+		core.CreateDocumentAt(".", false)
+
+	case "new":
+        var path string
+		if val := command.Args["path"].Value; val != "" {
+            path = val
+		} else {
+            path = command.Args["path"].DefaultValue
+		}
+        for key := range command.Flags {
+            switch key {
+            case "default":
+                core.CreateDocumentAt(path, true)
+                break commandLogic
+            }
+        }
+        core.CreateDocumentAt(path, false)
 	}
 }
