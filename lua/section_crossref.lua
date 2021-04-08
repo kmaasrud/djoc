65d5595ce090737ebc5e2098ee6a76d407b5a816
kmaasrud <km@aasrud.com>
2021-04-08 10:32:08 +0200

    Section cross-referencing fully functional like pandoc-crossref now

--- lua/section_crossref.lua
+++ lua/section_crossref.lua
@@ -1,41 +1,28 @@
-local function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
-end
+local sections = {}
 
-local make_sections = (require 'pandoc.utils').make_sections
-local section_numbers = {}
-
-function populate_section_numbers(doc)
-  function populate (elements)
+function find_sections(doc)
+  function populate(elements)
     for _, el in pairs(elements) do
       if el.t == 'Div' and el.attributes.number then
-        section_numbers['#' .. el.attr.identifier] = el.attributes.number
+        sections[el.attr.identifier] = el.attributes.number
         populate(el.content)
       end
     end
   end
 
-  populate(make_sections(true, nil, doc.blocks))
+  populate(pandoc.utils.make_sections(true, nil, doc.blocks))
 end
 
-function resolve_section_ref(link)
-  if table.unpack(link.content)["text"] ~= "*" or link.target:sub(1, 1) ~= '#' then
-    return nil
+function resolve_section_ref(cite)
+  id = cite.citations[1].id:match("^sec:.*")
+  if id then
+    link_text = pandoc.Str(sections[id])
+    return pandoc.Link({link_text}, '#' .. id, "", "")
   end
-  local section_number = pandoc.Str(section_numbers[link.target])
-  return pandoc.Link({section_number}, link.target, link.title, link.attr)
+  return nil
 end
 
 return {
   {Pandoc = populate_section_numbers},
-  {Link = resolve_section_ref}
+  {Cite = resolve_section_ref}
 }
