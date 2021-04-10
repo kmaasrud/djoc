5805137d4c408f8c2e956983fbf0fb8a551ebe06
kmaasrud <km@aasrud.com>
2021-04-10 18:45:41 +0200

    Simplified fetching of image ids and fixed latex refs

--- lua/crossref.lua
+++ lua/crossref.lua
@@ -20,7 +20,7 @@ local tables = {}
 local table_count = 1
 
 -- Whether to put parentheses around equation numbers or not.
--- This should be configured as a metadata thing, but hardcoding for now
+-- TODO: This should be configured as a metadata thing, but hardcoding for now
 local eq_surround_paren = false
 
 -- Poplate sections table with ID and number
@@ -37,13 +37,10 @@ function populate_sections(doc)
 end
 
 -- Populate figures table with ID and number
-function populate_figures(para)
-  if para.content[1].t == "Image" then
-    fig = para.content[1]
-    if fig.identifier and #fig.caption ~= 0 then
-      figures[fig.identifier] = figure_count
-      figure_count = figure_count + 1
-    end
+function populate_figures(fig)
+  if fig.identifier and #fig.caption ~= 0 then
+    figures[fig.identifier] = figure_count
+    figure_count = figure_count + 1
   end
 end
 
@@ -78,13 +75,13 @@ function populate_tables(table)
   if table.caption then
     -- Caption might contain several blocks, extract ID from last block
     caption = table.caption.long[#table.caption.long].content
-    for i=1,#caption do
-      j = #caption - i + 1 -- This is why indices start on 0...
+    for j=#caption,1,-1 do -- Iterate backwards to allow ID to be placed basically anywhere
       if caption[j].t == "Str" and caption[j].text:match("^{#tbl:.*}") then
-        id = caption[j].text:gsub("{#", ""):gsub("}", "") -- Strip the prefix and brackets
+        id = caption[j].text:gsub("{#", ""):gsub("}", "") -- Strip the prefix and brackets (FIXME: Other attributes should perhaps be allowed...)
         tables[id] = table_count
-        table_count = table_count + 1
+        table.identifier = id
         caption:remove(j); caption:remove(j-1) -- Remove ID definition and the space before
+        table_count = table_count + 1
         break
       end
     end
@@ -98,25 +95,24 @@ end
 function refs(cite)
   id = cite.citations[1].id
   sec = id:match("^sec:.*"); fig = id:match("^fig:.*"); tbl = id:match("^tbl:.*"); eq = id:match("^eq:.*")
-  if FORMAT == "latex" and sec and fig and tbl and eq then
-    return pandoc.RawInline("latex", "\\autoref{" .. id .. "}")
+  if FORMAT == "latex" and (sec or fig or eq) then
+    return pandoc.RawInline("latex", "\\ref{" .. id .. "}")
   elseif sec then
-    link_text = pandoc.Str(sections[id])
+    return pandoc.Link({pandoc.Str(sections[id])}, '#' .. id, "", "")
   elseif fig then
-    link_text = pandoc.Str(figures[id])
+    return pandoc.Link({pandoc.Str(figures[id])}, '#' .. id, "", "")
   elseif tbl then
-    link_text = pandoc.Str(tables[id])
+    return pandoc.Link({pandoc.Str(tables[id])}, '#' .. id, "", "")
   elseif eq then
-    link_text = pandoc.Str(equations[id])
+    return pandoc.Link({pandoc.Str(equations[id])}, '#' .. id, "", "")
   else
     return nil
   end
-  return pandoc.Link({link_text}, '#' .. id, "", "")
 end
 
 return {
   {Pandoc = populate_sections},
-  {Para = populate_figures},
+  {Image = populate_figures},
   {Para = populate_equations},
   {Table = populate_tables},
   {Cite = refs},
