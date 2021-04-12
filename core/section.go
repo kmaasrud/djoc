dc7a46a1706d8d350c181378b3b677352345e9c9
kmaasrud <km@aasrud.com>
2021-04-12 12:51:50 +0200

    Function to change the index of section

--- core/section.go
+++ core/section.go
@@ -4,17 +4,26 @@ import (
     "path/filepath"
     "strings"
     "strconv"
+    "fmt"
 )
 
+var SectionSep string = "_"
+
 type Section struct {
-    path string
-    title string
-    index int
+    Path string
+    Title string
+    Index int
+}
+
+func (s *Section) ChangeIndex(i int) {
+    s.Index = i
+    newFilename := fmt.Sprintf("%02d_", i) + strings.Join(strings.Split(filepath.Base(s.Path), SectionSep)[1:], "")
+    s.Path = filepath.Join(filepath.Dir(s.Path), newFilename)
 }
 
 func SectionFromPath(path string) (Section, error) {
-    split := strings.Split(strings.TrimSuffix(filepath.Base(path), filepath.Ext(path)), "_")
-    title := strings.Join(split[1:], "")
+    split := strings.Split(strings.TrimSuffix(filepath.Base(path), filepath.Ext(path)), SectionSep)
+    title := strings.Join(split[1:], "") // TODO: Find title from header of file
     index, err := strconv.Atoi(split[0])
     if err != nil {
         return Section{}, err
@@ -26,7 +35,7 @@ func SectionFromPath(path string) (Section, error) {
 func PathsFromSections(secs []Section) []string {
     var paths []string
     for _, sec := range secs {
-        paths = append(paths, sec.path)
+        paths = append(paths, sec.Path)
     }
     return paths
 }
