f1663d68be4f969cf6dd5e558dc9e72b82f66048
kmaasrud <km@aasrud.com>
2021-04-25 16:44:20 +0200

    Implemented new config in build command

--- cmd/build.go
+++ cmd/build.go
@@ -14,6 +14,7 @@ import (
 	"github.com/kmaasrud/doctor/msg"
 	"github.com/kmaasrud/doctor/utils"
 	"github.com/kmaasrud/doctor/core"
+	"github.com/kmaasrud/doctor/core/conf"
 	"github.com/kmaasrud/doctor/core/bib"
 )
 
@@ -51,25 +52,25 @@ func Build() error {
 
 	// Find config
 	msg.Info("Applying configuration from doctor.toml...")
-	conf, err := core.ConfigFromFile(filepath.Join(rootPath, "doctor.toml"))
+	config, err := conf.ConfigFromFile(filepath.Join(rootPath, "doctor.toml"))
 	if err != nil {
 		return err
 	}
 
     // Define output file
-    cmdArgs = append(cmdArgs, "-o", filepath.Join(rootPath, conf.Build.Filename+".pdf"))
+    cmdArgs = append(cmdArgs, "-o", filepath.Join(rootPath, config.Build.Filename+".pdf"))
 
 	// Add resource paths
 	resourcePaths := strings.Join([]string{rootPath, filepath.Join(rootPath, "assets"), filepath.Join(rootPath, "secs")}, utils.ResourceSep)
 	cmdArgs = append(cmdArgs, "--resource-path="+resourcePaths)
 
 	// Specify PDF engine and add options for specific engines
-	err = CheckPath(conf.Build.Engine)
+	err = CheckPath(config.Build.Engine)
 	if err != nil {
 		return errors.New("Build failed. " + err.Error())
 	}
-	cmdArgs = append(cmdArgs, fmt.Sprintf("--pdf-engine=%s", conf.Build.Engine))
-	if conf.Build.Engine == "tectonic" {
+	cmdArgs = append(cmdArgs, fmt.Sprintf("--pdf-engine=%s", config.Build.Engine))
+	if config.Build.Engine == "tectonic" {
         // Tectonic chatters a lot. Make it a bit more silent
 		cmdArgs = append(cmdArgs, "--pdf-engine-opt=-c=minimal")
 	}
@@ -84,7 +85,7 @@ func Build() error {
 	msg.Info(fmt.Sprintf("Found %d source files!", len(secs)))
 
 	// Temporarily write any Lua filters to file and add them to command
-	if conf.Build.LuaFilters {
+	if config.Build.LuaFilters {
 		msg.Info("Adding Lua filters...")
 		for filename, filter := range lua.Filters {
 			err := os.WriteFile(filepath.Join(rootPath, filename), filter, 0644)
@@ -103,12 +104,12 @@ func Build() error {
 		cmdArgs = append(cmdArgs, "-C", "--bibliography=references.bib")
         
         // If a CSL style is specified, make sure it exists in assets
-        if cslName := conf.Document.Csl; cslName != "" {
+        if cslName := config.Bib.Csl; cslName != "" {
             if val, ok := bib.Styles[cslName]; ok {
                 err := os.WriteFile(filepath.Join(rootPath, "assets", cslName + ".csl"), val, 0644)
                 if err != nil {
                     msg.Warning("Could not create CSL style, skipping it. " + err.Error())
-                    conf.Document.Csl = ""
+                    config.Bib.Csl = ""
                 }
             }
         }
@@ -116,7 +117,7 @@ func Build() error {
 
     // Write Pandoc's config options into a JSON file
 	jsonFilename := filepath.Join(rootPath, ".metadata.json")
-	err = conf.WritePandocJson(jsonFilename)
+	err = conf.WritePandocJson(jsonFilename, config)
 	if err != nil {
 		return err
 	}
