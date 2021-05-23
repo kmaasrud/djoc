ac47aef66b568ee65e53d06ac7f169c38683729c
kmaasrud <km@aasrud.com>
2021-05-23 09:57:47 +0200

    Help texts done - Closes #34

--- cmd/help.go
+++ cmd/help.go
@@ -33,7 +33,9 @@ Flags:
     --dependencies  Checks if all dependencies are installed
     --help          Shows this text
     --update        Checks if an update is available and offers to download it
-    --version       Prints the current version`,
+    --version       Prints the current version
+
+For more help on each sub-command, run 'doctor <command> --help'.`,
 
 	// ---
 	"add": `Add a new section to your Doctor document.
@@ -45,7 +47,28 @@ Options:
     --at            Specify which index the new section should have`,
 
 	// ---
-	"build": `Builds the current document according to doctor.toml`,
+	"build": `Builds the current document according to the instructions
+in doctor.toml.
+
+Usage:
+    doctor build`,
+
+	// ---
+	"list": `List all sections in the document, along with their index.
+
+Usage:
+    doctor list`,
+
+	// ---
+	"move": `Move a section to a new location in the document.
+
+Usage:
+    doctor move <section> <destination>
+
+<section> can be either the name or index of an existing section. <destination>
+is the index you want to move the selected section to.
+
+For an overview of the current document structure, you can run 'doctor list'.`,
 
 	// ---
 	"new": `Create a new Doctor document in the specified location.
@@ -60,4 +83,17 @@ document in the current directory.
 Options:
     --default       Initialize the document with a standard report type
                     document structure. Very limited at the moment.`,
+
+	// ---
+	"remove": `Removes a section, or multiple sections, from the document.
+
+Usage:
+    doctor remove <section(s)...> [options]
+
+The section(s) are specified either by their name or their index. For an 
+overview, you can run 'doctor list' to list all sections and their indices.
+
+Options:
+    --confirm       Do not ask for confirmation before deleting.
+                    Use this option with caution...`,
 }
