d86268b14951768851465c180a3124dc17d870a2
kmaasrud <km@aasrud.com>
2021-06-29 14:27:25 +0200

    Don't need the option to "process anyway"

--- lua/wordcount.lua
+++ lua/wordcount.lua
@@ -1,10 +1,9 @@
--- Thanks @jdutant @frederik-elwert and @jgm for this filter.
+-- Thanks @jdutant, @frederik-elwert and @jgm for this filter.
 -- Available in pandoc/lua-filters
 
 words = 0
 characters = 0
 characters_and_spaces = 0
-process_anyway = false
 
 wordcount = {
   Str = function(el)
@@ -37,21 +36,11 @@ wordcount = {
   end
 }
 
--- check if the `wordcount` variable is set to `process-anyway`
-function Meta(meta)
-  if meta.wordcount and (meta.wordcount=="process-anyway"
-    or meta.wordcount=="process" or meta.wordcount=="convert") then
-      process_anyway = true
-  end
-end
-
 function Pandoc(el)
     -- skip metadata, just count body:
     pandoc.walk_block(pandoc.Div(el.blocks), wordcount)
-    print(words .. " words in body")
-    print(characters .. " characters in body")
-    print(characters_and_spaces .. " characters in body (including spaces)")
-    if not process_anyway then
-      os.exit(0)
-    end
+    print("w: " .. words)
+    print("c: " .. characters)
+    print("cs: " .. characters_and_spaces)
+    os.exit(0)
 end
