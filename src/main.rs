489e21503b29a2b2cd574fe2d109d48dd946948b
kmaasrud <km@aasrud.com>
2021-10-22 17:23:30 +0200

    Basic app structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "doctor",
+    about = "Modern PDF creation through Markdown and LaTeX",
+    author = "Knut Magnus Aasrud",
+)]
+struct App {
+    #[structopt(subcommand)]
+    command: Command, 
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(about = "Builds a file or document")]
+    Build,
+
+    #[structopt(about = "Initializes a new document")]
+    Init,
+
+    #[structopt(about = "Lists the document structure")]
+    List,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::from_args();
+
+    match app.command {
+        Command::Build => {
+            println!("Building");
+        }
+
+        Command::Init => {
+            println!("Initializing");
+        }
+
+        Command::List => {
+            println!("Listing");
+        }
+    }
 }
