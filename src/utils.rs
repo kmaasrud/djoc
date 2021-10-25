b1c87131c4616427fcf3fe0108b97dbfbf77023b
kmaasrud <km@aasrud.com>
2021-10-25 19:11:24 +0200

    Dependencies for prev commit and better write_file

--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,23 @@
+use anyhow::{Result, Context};
 use std::fs::{self, File};
 use std::path::Path;
 use std::io::Write;
 
 /// Ease-of-use function for creating a file and writing bytes to it
-pub fn write_file(path: &Path, bytes: &[u8]) {
-    // Create file
+pub fn write_file(path: &Path, bytes: &[u8]) -> Result<()> {
+    // Ensure parent directory (if present)
     if let Some(p) = path.parent() {
-        fs::create_dir_all(p).unwrap();
+        fs::create_dir_all(p)
+            .with_context(|| format!("Could not create parent directory for {:?}", path))?;
     }
-    let mut f = File::create(path).unwrap();
+
+    // Create file
+    let mut f = File::create(path)
+        .with_context(|| format!("Could not create file from path {:?}", path))?;
 
     // Write bytes
-    f.write_all(bytes).unwrap();
+    f.write_all(bytes)
+        .with_context(|| format!("Error when writing bytes to {:?}", path))?;
+
+    Ok(())
 }
