89fb7171930a6d8c9cf82c19f3e4ae7bb97d1299
kmaasrud <km@aasrud.com>
2021-11-18 09:28:42 +0100

    Cleaned build function

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -1,13 +1,14 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use mdoc::{utils::write_file, DocumentBuilder};
 use std::path::{Path, PathBuf};
 
 pub fn build(file: Option<PathBuf>) -> Result<()> {
+    let builder = DocumentBuilder::new();
     let doc = match file {
-        Some(path) => DocumentBuilder::new()
+        Some(path) => builder
             .source(path)
             .build()?,
-        None => bail!("Can't load project yet"),
+        None => builder.build()?,
     };
 
     let pdf_data = doc.build()?;
--- src/document/document.rs
+++ src/document/document.rs
@@ -32,15 +32,15 @@ impl Document {
             .stdout(Stdio::piped())
             .spawn()?;
 
-        let stdin = pandoc.stdin.as_mut().context("Failed to open stdin")?;
+        let stdin = pandoc.stdin.as_mut().context("Failed to open stdin.")?;
 
         stdin
             .write_all(&self.content().as_bytes())
-            .context("Failed to write")?;
+            .context("Failed to write.")?;
 
         Ok(pandoc
             .wait_with_output()
-            .expect("Failed to read output")
+            .expect("Failed to read output.")
             .stdout)
     }
 
@@ -51,51 +51,47 @@ impl Document {
 
         let config = tectonic::config::PersistentConfig::open(false)
             .map_err(|e| Error::Tectonic(e))
-            .context("Failed to open the default configuration file")?;
+            .context("Failed to open the default configuration file.")?;
 
         let only_cached = false;
         let bundle = config
             .default_bundle(only_cached, &mut status)
             .map_err(|e| Error::Tectonic(e))
-            .context("Failed to load the default resource bundle")?;
+            .context("Failed to load the default resource bundle.")?;
 
         let format_cache_path = config
             .format_cache_path()
             .map_err(|e| Error::Tectonic(e))
-            .context("Failed to set up the format cache")?;
+            .context("Failed to set up the format cache.")?;
 
         let mut files = {
-            // Looking forward to non-lexical lifetimes!
             let mut sb = tectonic::driver::ProcessingSessionBuilder::default();
             sb.bundle(bundle)
                 .primary_input_buffer(&latex_bytes)
-                .tex_input_name("texput.tex")
+                .tex_input_name("mdoc.tex")
                 .format_name("latex")
                 .format_cache_path(format_cache_path)
-                .keep_logs(false)
-                .keep_intermediates(false)
-                .print_stdout(false)
                 .output_format(tectonic::driver::OutputFormat::Pdf)
                 .do_not_write_output_files();
 
             let mut sess = sb
                 .create(&mut status)
                 .map_err(|e| Error::Tectonic(e))
-                .context("Failed to initialize the LaTeX processing session")?;
+                .context("Failed to initialize the LaTeX processing session.")?;
 
             sess.run(&mut status)
                 .map_err(|e| Error::Tectonic(e))
-                .context("The LaTeX engine failed")?;
+                .context("The LaTeX engine failed.")?;
 
             sess.into_file_data()
         };
 
-        match files.remove("texput.pdf") {
+        match files.remove("mdoc.pdf") {
             Some(file) => Ok(file.data),
-            None => Err(Error::Io(std::io::Error::new(
+            None => Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
-                "LaTeX didn't report failure, but no PDF was created (??)",
-            ))
+                "LaTeX didn't report failure, but no PDF was created.",
+            )
             .into()),
         }
     }
