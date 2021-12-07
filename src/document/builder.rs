22ca623e67fb4a4be673c6df9c2ad05b632acb3c
kmaasrud <km@aasrud.com>
2021-12-07 20:46:36 +0100

    Added src field to config

--- src/document/builder.rs
+++ src/document/builder.rs
@@ -1,6 +1,6 @@
 use crate::{config::Config, utils::find_root, Chapter, Document};
 
-use anyhow::Result;
+use anyhow::{anyhow, bail, Context, Result};
 use ignore::{types::TypesBuilder, WalkBuilder};
 use std::path::{Path, PathBuf};
 
@@ -45,22 +45,29 @@ impl DocumentBuilder {
             SourceType::File(ref path) => {
                 (self.config.unwrap_or_default(), vec![Chapter::load(path)?])
             }
-            SourceType::Dir(ref path) => (
-                self.config
-                    .unwrap_or(Config::from_file(path.join("mdoc.toml"))?),
-                load_chapters(path),
-            ),
+
+            SourceType::Dir(ref path) => {
+                let config = self
+                    .config
+                    .unwrap_or(Config::from_file(path.join("mdoc.toml"))?);
+                let chapters = load_chapters(path, &config)?;
+
+                (config, chapters)
+            }
+
             SourceType::None => {
                 let root = find_root()?;
                 let config = self
                     .config
                     .unwrap_or(Config::from_file(root.join("mdoc.toml"))?);
-                (config, load_chapters(root))
+                let chapters = load_chapters(root, &config)?;
+
+                (config, chapters)
             }
         };
 
         if chapters.is_empty() {
-            anyhow::bail!("No chapters found.");
+            bail!("No chapters found.");
         }
 
         debug!("Building document with {} chapters.", chapters.len());
@@ -76,19 +83,46 @@ impl Default for DocumentBuilder {
     }
 }
 
-fn load_chapters<P: AsRef<Path>>(path: P) -> Vec<Chapter> {
-    let md_types = TypesBuilder::new()
-        .add_defaults()
-        .select("markdown")
-        .build()
-        .unwrap();
+fn load_chapters<P: AsRef<Path>>(path: P, config: &Config) -> Result<Vec<Chapter>> {
+    let walk = |path: &Path| {
+        let md_types = TypesBuilder::new()
+            .add_defaults()
+            .select("markdown")
+            .build()
+            .unwrap();
 
-    WalkBuilder::new(path)
-        .types(md_types)
-        .build()
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.path().is_file())
-        .map(|entry| Chapter::load(entry.path()))
-        .filter_map(|ch| ch.map_err(|err| warn!("{}. Skipping...", err)).ok())
-        .collect()
+        WalkBuilder::new(path)
+            .types(md_types)
+            .build()
+            .filter_map(|entry| entry.ok())
+            .filter(|entry| entry.path().is_file())
+            .map(|entry| Chapter::load(entry.path()))
+            .filter_map(|ch| ch.map_err(|err| warn!("{}. Skipping...", err)).ok())
+            .collect()
+    };
+
+    match &config.src {
+        Some(toml::Value::Array(paths)) => Ok(paths
+            .iter()
+            .filter_map(|val| val.as_str())
+            .map(|p| Chapter::load(Path::new(p)))
+            .filter_map(|ch| ch.map_err(|err| warn!("{}. Skipping...", err)).ok())
+            .collect()),
+
+        Some(toml::Value::String(path)) => {
+            let path = Path::new(&path);
+            if !path.exists() {
+                bail!("You specified a path {:?}, which does not exist.", path)
+            } else if !path.is_dir() {
+                Err(anyhow!(
+                    "The src field must be a directory or a list of files."
+                ))
+                .with_context(|| format!("{:?} is not a directory.", path))
+            } else {
+                Ok(walk(path))
+            }
+        }
+
+        _ => Ok(walk(path.as_ref())),
+    }
 }
