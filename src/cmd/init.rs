fddb5e0d660c447a7e8f75450953099c9c63a00a
kmaasrud <km@aasrud.com>
2021-12-16 09:47:27 +0100

    Added helpful comment to initial configuration file

--- src/cmd/init.rs
+++ src/cmd/init.rs
@@ -15,13 +15,20 @@ pub fn init(path: Option<PathBuf>) -> Result<()> {
     std::fs::create_dir_all(&root.join(SRC_DIR))
         .context("Failed at creating the directory structure.")?;
 
-    // Make default config with author name fetched from Git
-    let mut config = "title = \"Document title\"".to_string();
+    // Make default config
+    let mut config = String::new();
+    config.push_str(r#"# This is the configuration file of your document.
+# It is used to specify metadata, build instructions, styling and more."#);
+    config.push_str("\ntitle = \"Document title\"");
+
+    // Add author name from Git if available
     if let Some(author) = get_author_name() {
         config.push_str(&format!("\nauthors = [\"{}\"]", author))
     }
 
-    // Write config file
+    config.push_str("\n# For more options, visit https://kmaasrud.com/mdoc/config");
+
+    // Write to file
     write_file(&root.join(CONFIG_FILE), config.as_bytes())
         .context("Could not write configuration to file.")?;
 
