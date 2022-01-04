29355c8030af1531aba94c05f494823934643fd2
kmaasrud <km@aasrud.com>
2022-01-04 13:45:41 +0100

    Organize Lua filters in const array

--- src/pandoc/lua/mod.rs
+++ src/pandoc/lua/mod.rs
@@ -2,12 +2,14 @@ use crate::utils::{data_dir, write_file};
 use anyhow::Result;
 use std::path::PathBuf;
 
-const ABSTRACT: &[u8] = include_bytes!("abstract.lua");
-const CROSSREF: &[u8] = include_bytes!("crossref.lua");
+const FILTERS: [(&str, &[u8]); 2] = [
+    ("abstract.lua", include_bytes!("abstract.lua")),
+    ("crossref.lua", include_bytes!("crossref.lua"))
+];
 
 pub(crate) fn get_filters() -> Result<Vec<PathBuf>> {
     let mut paths = vec![];
-    for (data, fname) in [(ABSTRACT, "abstract.lua"), (CROSSREF, "crossref.lua")] {
+    for (fname, data) in FILTERS {
         let path = data_dir().join("filters").join(fname);
         if !path.exists() {
             write_file(&path, data)?;
