8ea12770f64ecb708ba2f8c37b06f42a26f90c5f
kmaasrud <km@aasrud.com>
2022-04-22 09:21:34 +0200

    fix: remove unwrap in get_csl fn

--- src/bib/csl.rs
+++ src/bib/csl.rs
@@ -5,39 +5,46 @@ use std::path::PathBuf;
 use url::Url;
 
 pub fn get_csl(id: &str) -> Result<PathBuf> {
-    // Safe unwrap: This function should only run in a valid MDoc document
-    let mut path = find_root().unwrap().join(id);
+    if let Ok(root) = find_root() {
+        // First check if id is relative path
+        let path = root.join(id);
+
+        if path.exists() {
+            return Ok(path);
+        }
+    }
+
+    let url = if let Ok(url) = Url::parse(id) {
+        // id is parsable URL, so use that URL
+        url
+    } else {
+        // id is the filestem of a CSL file contained in the citation-style-language/styles repo
+        Url::parse("https://raw.githubusercontent.com/citation-style-language/styles/master/")?
+            .join(&format!("{id}.csl"))?
+    };
+
+    let filename = url
+        .path_segments()
+        .ok_or_else(|| anyhow!("Could not determine the segments of \"{url}\".\nDoes your URL point to a valid CSL file?"))?
+        .last()
+        .ok_or_else(|| anyhow!("Unable to find the filename of \"{url}\".\nDoes your URL point to a valid CSL file?"))?;
+
+    let path = data_dir().join("csl").join(filename).with_extension("csl");
 
     if !path.exists() {
-        let url = if let Ok(url) = Url::parse(id) {
-            url
-        } else {
-            Url::parse("https://raw.githubusercontent.com/citation-style-language/styles/master/")?
-                .join(&format!("{id}.csl"))?
-        };
+        // If a cached CSL file doesn't exist, download it
+        info!(
+            "Fetching {:?} from \"{url}\" ...",
+            path.file_name().unwrap()
+        );
+        let resp = ureq::get(url.as_str()).call()?;
 
-        let filename = url
-            .path_segments()
-            .ok_or_else(|| anyhow!("Could not determine the segments of \"{url}\".\nDoes your URL point to a valid CSL file?"))?
-            .last()
-            .ok_or_else(|| anyhow!("Unable to find the filename of \"{url}\".\nDoes your URL point to a valid CSL file?"))?;
+        let mut bytes = Vec::new();
+        resp.into_reader()
+            .take(10_000_000)
+            .read_to_end(&mut bytes)?;
 
-        path = data_dir().join("csl").join(filename).with_extension("csl");
-
-        if !path.exists() {
-            info!(
-                "Fetching {:?} from \"{url}\" ...",
-                path.file_name().unwrap()
-            );
-            let resp = ureq::get(url.as_str()).call()?;
-
-            let mut bytes = Vec::new();
-            resp.into_reader()
-                .take(10_000_000)
-                .read_to_end(&mut bytes)?;
-
-            write_file(&path, &bytes)?;
-        }
+        write_file(&path, &bytes)?;
     }
 
     Ok(path)
