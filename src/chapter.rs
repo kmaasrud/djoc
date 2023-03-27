860797e941612d7394531bb638b4c99876158dd3
kmaasrud <km@aasrud.com>
2023-03-27 09:23:49 +0200

    feat: render math statically for HTML

--- src/chapter.rs
+++ src/chapter.rs
@@ -42,24 +42,40 @@ impl Chapter {
     fn get_parser(&self) -> impl Iterator<Item = Event> {
         // TODO: This is not ideal, as the mapping allocates quite a lot. When we can clone Events,
         // this will be a lot simpler, as we can just modify the events vector.
-        Parser::new(&self.content)
-            .flat_map(|event| match event {
-                Event::Start(
-                    Container::Div {
-                        class: Some("title"),
-                    },
-                    attrs,
-                ) => vec![
-                    Event::Start(Container::RawBlock { format: "latex" }, attrs.clone()),
-                    Event::Str(r"\maketitle".into()),
-                    Event::End(Container::RawBlock { format: "latex" }),
-                ],
-                _ => vec![event],
-            })
+        Parser::new(&self.content).flat_map(|event| match event {
+            Event::Start(
+                Container::Div {
+                    class: Some("title"),
+                },
+                attrs,
+            ) => vec![
+                Event::Start(Container::RawBlock { format: "latex" }, attrs),
+                Event::Str(r"\maketitle".into()),
+                Event::End(Container::RawBlock { format: "latex" }),
+            ],
+            _ => vec![event],
+        })
     }
 
     pub fn write_html<W: fmt::Write>(&self, w: W) -> fmt::Result {
-        html::Renderer.push(self.get_parser(), w)
+        let mut in_math = false;
+        let mut opts = katex::Opts::default();
+        let parser = self.get_parser().map(|event| match event {
+            Event::Start(Container::Math { display }, attrs) => {
+                opts.set_display_mode(display);
+                in_math = true;
+                Event::Start(Container::RawBlock { format: "html" }, attrs)
+            }
+            Event::End(Container::Math { .. }) => {
+                in_math = false;
+                Event::End(Container::RawBlock { format: "html" })
+            }
+            Event::Str(s) if in_math => {
+                Event::Str(katex::render_with_opts(&s, &opts).unwrap().into())
+            }
+            _ => event,
+        });
+        html::Renderer::default().push(parser, w)
     }
 
     pub fn write_latex<W: fmt::Write>(&self, w: W) -> fmt::Result {
